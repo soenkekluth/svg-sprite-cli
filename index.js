d65864fe00d43cb40ecbda7bc1e2450ea3a4e7ac
Sönke Kluth <mail@soenkekluth.com>
2017-06-26 12:46:42 +0200

    initial

---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2));
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+const gulp = require('gulp');
+const svgSprite = require('gulp-svg-sprites');
+var filter    = require('gulp-filter');
+var svg2png   = require('gulp-svg2png');
+
+
+const template = fs.readFileSync("./template/sprite.scss", "utf-8");
+
+var src;
+var dest;
+
+var input = argv._;
+
+src = path.resolve(input[0] || '.');
+dest = input[1] ? path.resolve(input[1]) : src;
+
+if(!(fs.existsSync(src) || fs.existsSync(dest))){
+  throw ('path not valid');
+  process.abort();
+  return;
+}
+// console.log(src, dest, argv);
+
+var config = argv.config || {
+
+    mode: "sprite",
+    common: "svg-icon",
+    templates: {
+      scss: template
+    },
+    // cssFile: 'scss/_sprite.scss',
+    // svg: {
+    //   sprite: 'img/sprite.svg'
+    // },
+    // preview: false,
+    padding: 10,
+
+    selector: "%f"
+};
+
+
+
+
+gulp.task('default', function() {
+  return gulp.src(src + '/*.svg')
+  // .pipe(svgSprite({mode: "defs"}))
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest(dest))
+    .pipe(filter('**/*.svg'))
+    .pipe(svg2png())
+    .pipe(gulp.dest(dest))
+});
+
+
+gulp.start('default');
